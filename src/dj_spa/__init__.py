9b2641ee5f97dffe56ace999a11393002505271d
Ricardo Robles <ricardo.r.f@hotmail.com>
2026-07-08 10:53:26 +0200

    Add async_mode parameter to dj_spa function

--- src/dj_spa/__init__.py
+++ src/dj_spa/__init__.py
@@ -1,9 +1,11 @@
 __all__ = ["dj_spa"]
 
+from typing import Callable
+
 from django.urls import re_path
 from django.urls.resolvers import URLPattern
 
-from .views import CacheControl, spa_view
+from .views import CacheControl, async_spa_view, spa_view
 
 
 def dj_spa(
@@ -13,12 +15,14 @@ def dj_spa(
     error_400: str | None = None,
     error_500: str | None = None,
     cache_control: CacheControl = None,
+    async_mode: bool = False,
 ) -> URLPattern:
     prefix = prefix.rstrip("/")
     regex = rf"^{prefix}/(?P<path>.*)$"
+    view: Callable = async_spa_view if async_mode else spa_view
     return re_path(
         regex,
-        spa_view,
+        view,
         kwargs={
             "dist_dir": dist_dir,
             "entry_point": entry_point,
